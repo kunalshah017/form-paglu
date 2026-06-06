09a9f6950c010db581a18f2ef291de5d8919cf2d
kunalshah017 <kunalmanishshah@gmail.com>
2026-06-06 23:51:42 +0530

    chore: clean up boilerplate - keep only popup & side-panel

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,9 +1,3 @@
 import 'webextension-polyfill';
-import { exampleThemeStorage } from '@extension/storage';
 
-exampleThemeStorage.get().then(theme => {
-  console.log('theme', theme);
-});
-
-console.log('Background loaded');
-console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
+console.log('Background service worker loaded');
